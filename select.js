83298eda678370790a7f6ae38289daa5315f6706
yaya4red <yaya.hung@itrue.com.tw>
2014-01-08 12:24:47 +0800

    pink不重覆加

--- select.js
+++ select.js
@@ -1,11 +1,13 @@
 var color = $(".color");
 color.hide();
 $("button[name='add']").on("click",function() {
-	var yellow = $("select option").filter("[value='2']");
-	yellow.after("<option value='4'>Pink</option>");
+  if ($("[value ='4']").length == 0) {
+    var yellow = $("select option").filter("[value='2']");
+    yellow.after("<option value='4'>Pink</option>");
+  } 
 });
 $("button[name='remove']").on("click",function() {
-	$("select option").filter("[value='4']").remove();
+  $("select option").filter("[value='4']").remove();
 });
 $("select").on("change", function() {
 	var ovalue = $(this).val();
