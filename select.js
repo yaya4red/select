da6598f2d051ba951d556166bc225a7b1a60e0ef
yaya4red <yaya.hung@itrue.com.tw>
2014-01-09 15:52:15 +0800

    chosen版追加

--- select.js
+++ select.js
@@ -1,17 +1,36 @@
-var color = $(".color");
+var color = $(".content").find(".color");
 color.hide();
-$("button[name='add']").on("click",function() {
+$(".chosen-select").chosen({disable_search_threshold: 10});
+$("button[name='add1']").on("click",function() {
   if ($("[value ='4']").length == 0) {
     var yellow = $("select option").filter("[value='2']");
     yellow.after("<option value='4'>Pink</option>");
   } 
 });
-$("button[name='remove']").on("click",function() {
+$("button[name='remove1']").on("click",function() {
   $("select option").filter("[value='4']").remove();
+});
+$("button[name='add']").on("click",function() {
+  if ($("[value ='Pink']").length == 0) {
+    var yellow = $("select option").filter("[value='Yellow']");
+    yellow.after("<option value='Pink'>Pink</option>");
+    $(".chosen-select").trigger("chosen:updated");
+  } 
+});
+$("button[name='remove']").on("click",function() {
+  $("select.chosen-select").find("option").filter("[value='Pink']").remove();
+  $(".chosen-select").trigger("chosen:updated");
 });
 $("select").on("change", function() {
 	var ovalue = $(this).val();
 	var showcolor = $(".color[name="+ ovalue +"]");
 	showcolor.fadeIn("slow");
 	showcolor.siblings().fadeOut();
+});
+$("select.chosen-select").on("change", function() {
+  var ovalue = $(this).val();
+  var showcolor = $(".color[name="+ ovalue +"]");
+  showcolor.fadeIn("slow");
+  showcolor.siblings().fadeOut();
+  $(".chosen-select").trigger("chosen:updated");
 });
